3f09d97916936beb4164bceaaf8408faa66ba763
Lujan Maccari <mlujanmaccari@gmail.com>
2023-06-14 22:49:18 -0300

    first commit

---
+++ coder-vue.js/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="container">
+      <LoginForm msg="Bienvenido!" />
+    </div>
+  </header>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue'
+export default {
+  name: 'App',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style scoped>
+
+.container{
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20vh;
+}
+</style>
---
+++ coder-vue.js/src/components/LoginForm.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="container">
+    <h1>{{ msg }}</h1>
+    <div>
+      <form className="form">
+        <div>
+          <label for="email" class="text">Email</label>
+          <input v-model="email" type="email" class="input" id="email" placeholder="Email" />
+        </div>
+        <div>
+          <label for="password" class="text">Contraseña</label>
+          <input
+            v-model="password"
+            type="password"
+            class="input"
+            id="password"
+            placeholder="Contraseña"
+          />
+        </div>
+        <button @click="login" class="btn">Iniciar sesión</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+    login() {
+      // Lógica de inicio de sesión
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  background-color: aliceblue;
+  border-radius: 20px;
+  max-width: 400px;
+  padding: 200px;
+  display: flex;
+  flex-direction: column;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+
+  justify-content: space-evenly;
+  height: 40vh;
+}
+.btn{
+  margin-top: 20px;
+  border: none;
+  border-radius: 10px;
+  background-color:rgba(111, 175, 230, 0.452);
+  padding: 8px;
+  cursor: pointer
+}
+.btn:hover{
+  background-color: rgba(111, 175, 230, 0.603);
+}
+.input{
+  border: none;
+  border-radius: 10px;
+  padding: 5px;
+  background-color: rgba(205, 230, 252, 0.425);
+}
+.text{
+  font-size: 15px;
+  cursor: default
+}
+</style>
