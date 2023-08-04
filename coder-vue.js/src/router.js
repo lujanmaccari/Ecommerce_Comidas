0a755e4fad5d32acb015f78ea01a15795156b7b5
Lujan Maccari <mlujanmaccari@gmail.com>
2023-08-04 18:43:15 -0300

    bloqueo de rutas

--- coder-vue.js/src/router.js
+++ coder-vue.js/src/router.js
@@ -13,7 +13,7 @@ const routes = [
   { path: '/cart', component: CartComponent, name: 'cart' },
   { path: '/user-panel', component: UserPanel, name: 'user-panel' },
   { path: '/admin-panel', component: AdminPanel, name: 'admin-panel' },
-  { path: '/gestion-pedidos', component: GestionPedidos, name: 'gestion-pedidos' },
+  { path: '/gestion-pedidos', component: GestionPedidos, name: 'gestion-pedidos' }
 ]
 
 const router = createRouter({
@@ -21,4 +21,17 @@ const router = createRouter({
   routes: routes
 })
 
+const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+
+router.beforeEach((to, from, next) => {
+  if (to.path.includes('admin') && !userInfo?.isAdmin) {
+    next({ name: 'login' })
+  } else if (to.path.includes('user') && userInfo?.isAdmin) {
+    next({ name: 'login' })
+  } else if (to.path.includes('cart') && !userInfo) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
+})
 export default router
